33c69cce0f00cd6ee7df0b6f1345f377a621b2e1
Bofan Chen <bofanchen057@gmail.com>
2024-11-18 16:52:19 -0500

    11/18
    adds nav bar

--- app.py
+++ app.py
@@ -11,24 +11,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/data')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data', methods=['GET'])
 def get_sensor_data():
     return jsonify(sensor_data)
 
 
 @app.route('/data', methods=['POST'])
 def receive_data():
-    """
-    Endpoint for Pico W devices to report sensor data.
-    Example Payload:
-    {
-        "device_id": "sensor_1",
-        "readings": {
-            "temperature": 20,
-            "humidity": 10
-        }
-    }
-    """
+
+    # Endpoint for Pico W devices to report sensor data.
+    # Example Payload:
+    # {
+    #     "device_id": "sensor_1",
+    #     "readings": {
+    #         "temperature": 20,
+    #         "humidity": 10
+    #     }
+    # }
+
     data = request.json
     device_id = data.get('device_id')
     readings = data.get('readings')  # This should be a dictionary of readings
@@ -48,4 +52,4 @@ def receive_data():
 
 if __name__ == '__main__':
     # Set host to '0.0.0.0' to make the server accessible externally
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
